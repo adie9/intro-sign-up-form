ede4d84746606d8ebe218c6bfd3a7e13647a3697
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-03-25 12:27:38 -0400

    added basic test javascript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+    $(".claim").click(function(){
+        $("form input").each(function() {
+            let input = $(this);
+            if (!input.val()) {
+                alert("Type " + input.attr("type") + "Name " + input.attr("name"));
+            }
+            
+        })
+    })
     
 });
