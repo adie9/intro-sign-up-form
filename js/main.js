09431baa1fe52f0c4447676bc982170fcad005b2
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-03-28 12:13:56 -0400

    minor fixes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
     $(".claim").click(function(){
         $("form input").each(function() {
             let input = $(this);
+            let name = input.attr("name");
+            let error_msg = name + ' cannot be empty.';
+            
+
             if (!input.val()) {
                 $("input:invalid").css({"border-color":"red", 
                 "background":"url(../images/icon-error.svg)",
